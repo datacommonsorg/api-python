08f4633d0da331953b93c55dc154d19d60aa58d1
Prashanth Radhakrishnan <shanth@google.com>
2018-09-25 22:22:23 -0700

    Move tests and examples into separate directories.

--- datacommons/datacommons.py
+++ datacommons/datacommons.py
@@ -16,7 +16,7 @@
 """
 
 from collections import defaultdict
-from ._auth import do_auth
+from . import _auth
 import pandas as pd
 
 
@@ -33,7 +33,7 @@ class Client(object):
                client_id=_CLIENT_ID,
                client_secret=_CLIENT_SECRET,
                api_root=_API_ROOT):
-    self._service = do_auth(client_id, client_secret, api_root)
+    self._service = _auth.do_auth(client_id, client_secret, api_root)
     self._inited = True
 
   def Query(self, datalog_query):
---
+++ datacommons/examples/simple_query.py
@@ -0,0 +1,46 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Example client for DataCommons Python API.
+
+"""
+
+import datacommons
+import pandas as pd
+
+
+def main():
+  dc = datacommons.Client()
+
+  # Get lat/long of a city.
+  query = ("""
+           SELECT ?id ?lat ?long,
+             typeOf ?o City,
+             name ?o 'San Luis Obispo',
+             dcid ?o ?id,
+             latitude ?o ?lat,
+             longitude ?o ?long
+           """)
+  print 'Issuing query "{}"'.format(query)
+  try:
+    df = dc.Query(query)
+  except RuntimeError as e:
+    print e
+    return
+
+  with pd.option_context('display.width', 400, 'display.max_rows', 100):
+    print df
+
+
+if __name__ == '__main__':
+  main()
