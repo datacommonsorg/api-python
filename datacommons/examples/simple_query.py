ef6a13d0f35c0fafcaf8358d4889e2e3b0bec134
Bo Xu <boxu@google.com>
2018-11-14 15:56:35 -0800

    Use updated caching api

--- datacommons/examples/simple_query.py
+++ datacommons/examples/simple_query.py
@@ -45,8 +45,9 @@ def main():
   with pd.option_context('display.width', 400, 'display.max_rows', 100):
     print(df)
 
-  dc.save_dataframe(df, 'test_df')
-  saved_df = dc.read_dataframe('test_df')
+  saved_file_name = dc.save_dataframe(df, 'test_df')
+  print(saved_file_name)
+  saved_df = dc.read_dataframe(saved_file_name)
   assert df.equals(saved_df)
 
 
