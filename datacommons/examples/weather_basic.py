fb2b0e8d6bbb8c9e1def5513fa739efc8e76498f
Antares C <antares21chen@gmail.com>
2019-06-21 10:47:16 -0700

    feature/api-version-2 (#47)
    * Corrected namespace for Intangible, hyperlinked a definition.
    
    * Added schema.datacommons.org/County.
    
    * Added documentation for dcid and provenance.
    
    * Added documentation for area, timezone, freebaseId, geonamesId, and wikidataId.
    
    * Fixed name of geonamesId.
    
    * Removed 'current' from the definition of timezone.
    
    * Fixed the empty column bug (#24)
    
    * Add GNIS property.
    
    * Add  fipsId property.
    
    * Use updated caching api
    
    * Allow space in column name
    
    * Remove print
    
    * Fixed <br> to <br/> for xmllint (our rdf parsing is xhtml-based).
    (we may need to handle html in comments differently too)
    
    * Preparation for use with sdoapp
    
    * tweaks
    
    * Test of changes visibility
    
    * Test changes
    
    * Added config file to repo
    
    * Mods to roduce correct (vocabUri based) RDFa
    
    * Update datacommons.py
    
    Add a 'typeOf' literal when querying for the type of the container place for get_places_in().
    
    * Add missing "()" to a query in get_places_in().
    
    * Update datacommons client to be compatible with new schema
    
    * Added ability to specify childhoodLocation (#34)
    
    * Added ability to specify childhoodLocation
    
    * Never change python
    
    * Add CensusTract to get_places_in API
    
    * Handle error case
    
    * Add comment
    
    * added empty examples file for consistancy
    
    * Adjust config for release
    
    * Added temporary test config
    
    * Add deploymeny yaml files
    
    * Added draft deployment instructions file
    
    * Change deployment file from html to md
    
    * For observations, use observation_date instead of start_time/end_time
    
    * Use prod client
    
    * Use orient split in read and save dataframe so index is not saved in json
    
    * Add measurementMethod in get_observation
    
    * Update client library to fit for new mixer string_value
    
    * Fix header prefix and date format
    
    * docstring typos and consistency
    
    * Reimplemented base API
    
    * Implemented places extension
    
    * Fixed bug
    
    * Added bio stub
    
    * Finished re-implementing bio extension... n o w t o t e s t.
    
    * Added examples and fixed DCFrame bugs
    
    * added populations stub for pop extension
    
    * implemented get_pop and get_obs in populations API n o w t o t e s t
    
    * Fixed dangling line in places
    
    * BioExtension demo works
    
    * Some tweaks
    
    * Fixed header comment
    
    * missing comma
    
    * fixed typo self._col_type -> self._col_types
    
    * seed and new col val already have ? append to beg
    
    * fixing populations library and updating infra
    
    * similar fix for get_obs for extra ? for colvar
    
    should have fixed this when I fixed get_populations...
    
    * get useful prints in test/examples
    
    * fat fingers on copy and paste
    
    * places.py self._col_type -> self._col_types
    
    * Implemented draft of weather API extension
    
    * Added weather example
    
    * Weather API works
    
    * Added bio mixer specs and tweaked bio API
    
    * Fixed places bug

---
+++ datacommons/examples/weather_basic.py
@@ -0,0 +1,75 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Basic demo showing how to use the weather extension
+"""
+
+from datacommons.utils import DatalogQuery
+from datacommons.weather import WeatherExtension
+from datacommons.utils import MeasuredValue
+
+import pandas as pd
+
+import datacommons
+
+# Print options
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 20)
+
+def display_table(num, label, df):
+  print('-'*80)
+  print('EXAMPLE {}: {}'.format(num, label))
+  print('-'*80 + '\n')
+  print(df)
+  print('\n')
+
+def main():
+  # Create a list of places to get weather.
+  places = [
+      'geoId/4261000', # Pittsburgh, PA
+      'geoId/0649670', # Mountain View, CA
+      'geoId/4805000', # Austin, TX
+      'geoId/0606000', # Berkeley, CA
+  ]
+
+  # Example 1: Getting the temperature
+  frame_1 = datacommons.DCFrame()
+  frame_1 = WeatherExtension(frame_1)
+  frame_1.add_column('CityID', 'City', places)
+  frame_1.expand('name', 'CityID', 'CityName', new_col_type='Text')
+  frame_1.get_temperature('CityID', 'MeanTemp', MeasuredValue.MEAN, date='2019-05-09')
+  frame_1.get_temperature('CityID', 'MinTemp', MeasuredValue.MIN, date='2019-05-09')
+  display_table(1, 'Temperature', frame_1.pandas())
+
+  # Example 2: Getting the rainfall
+  frame_2 = datacommons.DCFrame()
+  frame_2 = WeatherExtension(frame_2)
+  frame_2.add_column('CityID', 'City', places)
+  frame_2.expand('name', 'CityID', 'CityName', new_col_type='Text')
+  frame_2.get_rainfall('CityID', 'MeanRain', MeasuredValue.MEAN, date='2019-05-09')
+  frame_2.get_rainfall('CityID', 'MinRain', MeasuredValue.MIN, date='2019-05-09')
+  display_table(2, 'Rainfall', frame_2.pandas())
+
+  # Example 3: Getting the visibility
+  frame_3 = datacommons.DCFrame()
+  frame_3 = WeatherExtension(frame_3)
+  frame_3.add_column('CityID', 'City', places)
+  frame_3.expand('name', 'CityID', 'CityName', new_col_type='Text')
+  frame_3.get_visibility('CityID', 'MeanVisibility', MeasuredValue.MEAN, date='2019-05-09')
+  display_table(3, 'Visibility', frame_3.pandas())
+
+
+if __name__ == '__main__':
+  main()
---
+++ datacommons/utils.py
@@ -0,0 +1,91 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" DataCommons utilities library
+
+Contains various functions that can aid in the extension of the DataCommons API.
+"""
+
+from collections import OrderedDict
+
+
+class MeasuredValue:
+  """ An enumeration of valid measured values in the DataCommons graph.
+
+  A measured value in a Statistical Observation is the type of statistic being
+  measured for example: "mean", "min", "max", etc. The associated string field
+  is the DataCommons property used to query for the measured value.
+  """
+  MIN = 'minValue'
+  MAX = 'maxValue'
+  MEAN = 'meanValue'
+
+
+class DatalogQuery(object):
+  """ A class wrapping a DataCommons datalog query string. """
+
+  def __init__(self):
+    self._variables = []
+    self._constraints = OrderedDict()
+
+  def __str__(self):
+    """ Returns the query stored by this object. """
+    query_body = ''
+    for sub in self._constraints:
+      for pred in self._constraints[sub]:
+        for obj in self._constraints[sub][pred]:
+          if isinstance(obj, list):
+            obj_vals = ' '.join(obj).strip()
+            query_body += '{} {} {},\n'.format(pred, sub, obj_vals)
+          else:
+            query_body += '{} {} {},\n'.format(pred, sub, obj)
+    query = 'SELECT {},\n{}'.format(' '.join(self._variables), query_body)
+    return query
+
+  def variables(self):
+    """ Returns the set of variables. """
+    return self._variables
+
+  def var_types(self):
+    """ Returns a map from query variable to types specified in the query. """
+    # Get initial var types
+    var_types = {}
+    for sub in self._constraints:
+      for pred in self._constraints[sub]:
+        if pred == 'typeOf':
+          var_types[sub] = self._constraints[sub][pred][0]
+
+    # If the property is dcid, then the type can be carried over
+    for sub in self._constraints:
+      for pred in self._constraints[sub]:
+        if pred == 'dcid':
+          for obj in self._constraints[sub][pred]:
+            if isinstance(obj, str) and sub in var_types:
+              var_types[obj] = var_types[sub]
+    return var_types
+
+  def add_variable(self, *variables):
+    """ Add variables to the query. """
+    for var in variables:
+      if var not in self._variables:
+        # Maintaining order of the variables is important
+        self._variables.append(var)
+
+  def add_constraint(self, sub, pred, obj):
+    """ Add constraints to the query. """
+    # Entries in the set correspond to separate lines
+    if sub not in self._constraints:
+      self._constraints[sub] = OrderedDict()
+    if pred not in self._constraints[sub]:
+      self._constraints[sub][pred] = []
+    self._constraints[sub][pred].append(obj)
