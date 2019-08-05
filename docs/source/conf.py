83f6759e869f603435b61e614fcf1d83cdacfd33
Antares Chen <antares21chen@gmail.com>
2019-08-05 16:14:34 -0700

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,11 +27,13 @@ release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.napoleon']
-# extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
