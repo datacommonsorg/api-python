32a028486c8b4b4d1388b96b16a41c8572a4a06c
tjann <18621425+tjann@users.noreply.github.com>
2020-08-24 02:16:06 -0700

    Do the python release in another PR.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ URL = 'https://github.com/datacommonsorg/api-python'
 EMAIL = 'support@datacommons.org'
 AUTHOR = 'datacommons.org'
 REQUIRES_PYTHON = '>=2.7'
-VERSION = '1.4.0'
+VERSION = '1.3.0'
 
 REQUIRED = [
     'six',
