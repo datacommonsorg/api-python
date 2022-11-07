d9cf029d41cabd411e8e01027f1b6a8cc5fb0cb7
Bo Xu <shifucun@users.noreply.github.com>
2022-11-07 13:20:13 -0800

    Add barebone APIs for observation and event

---
+++ datacommons/observation.py
@@ -0,0 +1,30 @@
+# Copyright 2022 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" API to get statistical observation data."""
+
+
+def observation_series():
+    return None
+
+
+def observation_series_of_entities_within_place():
+    return None
+
+
+def observation_point():
+    return None
+
+
+def observation_point_of_entities_within_place():
+    return None
