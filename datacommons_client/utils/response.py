1b29cf6ff04e39548b52a6d254002027bbaa8c82
Jorge Rivera <jorge.rivera@one.org>
2024-12-05 15:19:36 +0100

    ObservationResponse

--- datacommons_client/utils/response.py
+++ datacommons_client/utils/response.py
@@ -1,7 +1,13 @@
 from dataclasses import asdict, dataclass, field
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from datacommons_client.models.node import Arcs, NextToken, NodeDCID, Properties
+from datacommons_client.models.observation import (
+    Facet,
+    Variable,
+    facetID,
+    variableDCID,
+)
 
 
 @dataclass
@@ -107,3 +113,115 @@ def _unpack_arcs(arcs: Dict[str, Any]) -> Any:
     for property_data in arcs.values():
         nodes = property_data.get("nodes", [])
         return nodes if len(nodes) > 1 else (nodes[0] if nodes else None)
+
+
+@dataclass
+class ObservationResponse:
+    """Represents the response from the Data Commons API Observation endpoint.
+
+    Attributes:
+       byVariable: A dictionary of variable DCIDs and their corresponding data.
+         facets: A dictionary of facet IDs and their corresponding data.
+    """
+
+    byVariable: Dict[variableDCID, Any] = field(default_factory=dict)
+    facets: Dict[facetID, Any] = field(default_factory=dict)
+
+    @classmethod
+    def from_json(cls, json_data: Dict[str, Any]) -> "ObservationResponse":
+        """Parses the data from the API response."""
+        return cls(
+            byVariable={
+                variable: Variable.from_json(data)
+                for variable, data in json_data.get("byVariable", {}).items()
+            },
+            facets={
+                facet: Facet.from_json(data)
+                for facet, data in json_data.get("facets", {}).items()
+            },
+        )
+
+    @property
+    def json(self):
+        return asdict(self)
+
+    def get_data_by_entity(self) -> Dict:
+        """Unpacks the data for each entity, for each variable.
+
+        Returns:
+            Dict: The variables object from the response.
+        """
+        return {
+            variable: data.byEntity
+            for variable, data in self.byVariable.items()
+        }
+
+    def get_observations_as_records(self) -> List[Dict[str, Any]]:
+        """Converts the observation data into a list of records.
+
+        Returns:
+            List[Dict[str, Any]]: A flattened list of observation records.
+        """
+        return observations_as_records(
+            data=self.get_data_by_entity(), facets=self.facets
+        )
+
+
+def extract_observations(
+    variable: str, entity: str, entity_data: dict, facet_metadata: dict
+) -> list[dict]:
+    """
+    Extracts observations for a given variable, entity, and its data.
+
+    Args:
+        variable (str): The variable name.
+        entity (str): The entity name.
+        entity_data (dict): Data for the entity, including ordered facets.
+        facet_metadata (dict): Metadata for facets.
+
+    Returns:
+        list[dict]: A list of observation records.
+    """
+    # Store observation records
+    records = []
+
+    # Extract observations
+    for facet in entity_data.get("orderedFacets", []):
+        facet_id = facet.facetId
+        metadata = facet_metadata.get(facet_id, {})
+        records.extend(
+            {
+                "date": observation.date,
+                "entity": entity,
+                "variable": variable,
+                "value": observation.value,
+                "facetId": facet_id,
+                **asdict(metadata),
+            }
+            for observation in facet.observations
+        )
+    return records
+
+
+def observations_as_records(data: dict, facets: dict) -> list[dict]:
+    """
+    Converts observation data into a list of records.
+
+    Args:
+        data (dict): A mapping of variables to entities and their data.
+        facets (dict): Facet metadata for the observations.
+
+    Returns:
+        list[dict]: A flattened list of observation records.
+    """
+    return [
+        record
+        for variable, entities in data.items()
+        for entity, entity_data in entities.items()
+        for record in extract_observations(
+            variable=variable,
+            entity=entity,
+            entity_data=entity_data,
+            facet_metadata=facets,
+        )
+    ]
