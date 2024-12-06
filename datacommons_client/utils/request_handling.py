bbef336e1c490d5aee4e4de6aee2a648b5774124
Jorge Rivera <jorge.rivera@one.org>
2024-12-06 12:55:06 +0100

    Create request_handling.py
    Validates instance.
    Builds header.
    Manages post requests, including multi-page responses

--- datacommons_client/utils/request_handling.py
+++ datacommons_client/utils/request_handling.py
@@ -251,7 +251,7 @@ def post_request(
     headers: dict[str, str],
     max_pages: Optional[int] = None,
 ) -> Dict[str, Any]:
-    """Send a POST request with optional pagination support and return a single dictionary.
+    """Send a POST request with optional pagination support and return a DCResponse.
 
     Args:
         url: The target endpoint URL.
