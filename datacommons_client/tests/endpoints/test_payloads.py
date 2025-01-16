de862a42283a770414ee49db35ded5da22a9e18c
Jorge Rivera <jorge.rivera@one.org>
2025-01-16 14:55:53 -0600

    Add *Request to Payload class names for clarity
    Renames all Payload classes and clarifies that they are request payloads.

--- datacommons_client/tests/endpoints/test_payloads.py
+++ datacommons_client/tests/endpoints/test_payloads.py
@@ -1,34 +1,34 @@
 import pytest
 
-from datacommons_client.endpoints.payloads import NodePayload
+from datacommons_client.endpoints.payloads import NodeRequestPayload
 from datacommons_client.endpoints.payloads import ObservationDate
-from datacommons_client.endpoints.payloads import ObservationPayload
-from datacommons_client.endpoints.payloads import ResolvePayload
+from datacommons_client.endpoints.payloads import ObservationRequestPayload
+from datacommons_client.endpoints.payloads import ResolveRequestPayload
 
 def test_node_payload_normalize():
-    """Tests that NodePayload correctly normalizes single and multiple nodes."""
-    payload = NodePayload(nodes="node1", prop="prop1")
+    """Tests that NodeRequestPayload correctly normalizes single and multiple nodes."""
+    payload = NodeRequestPayload(nodes="node1", prop="prop1")
     assert payload.nodes == ["node1"]
 
-    payload = NodePayload(nodes=["node1", "node2"], prop="prop1")
+    payload = NodeRequestPayload(nodes=["node1", "node2"], prop="prop1")
     assert payload.nodes == ["node1", "node2"]
 
 
 def test_node_payload_validate():
-    """Tests that NodePayload validates its inputs correctly."""
+    """Tests that NodeRequestPayload validates its inputs correctly."""
     with pytest.raises(ValueError):
-        NodePayload(nodes="node1", prop=123)  # `prop` must be a string
+        NodeRequestPayload(nodes="node1", prop=123)  # `prop` must be a string
 
 
 def test_node_payload_to_dict():
-    """Tests NodePayload conversion to dictionary."""
-    payload = NodePayload(nodes="node1", prop="prop1")
+    """Tests NodeRequestPayload conversion to dictionary."""
+    payload = NodeRequestPayload(nodes="node1", prop="prop1")
     assert payload.to_dict == {"nodes": ["node1"], "property": "prop1"}
 
 
 def test_observation_payload_normalize():
-    """Tests that ObservationPayload normalizes inputs correctly."""
-    payload = ObservationPayload(
+    """Tests that ObservationRequestPayload normalizes inputs correctly."""
+    payload = ObservationRequestPayload(
         date="LATEST",
         variable_dcids="var1",
         select=["variable", "entity"],
@@ -38,7 +38,7 @@ def test_observation_payload_normalize():
     assert payload.entity_dcids == ["ent1"]
     assert payload.date == ObservationDate.LATEST
 
-    payload = ObservationPayload(
+    payload = ObservationRequestPayload(
         date="all",
         variable_dcids=["var1"],
         select=["variable", "entity"],
@@ -50,9 +50,9 @@ def test_observation_payload_normalize():
 
 
 def test_observation_payload_validate():
-    """Tests that ObservationPayload validates its inputs."""
+    """Tests that ObservationRequestPayload validates its inputs."""
     with pytest.raises(ValueError):
-        ObservationPayload(
+        ObservationRequestPayload(
             date="LATEST",
             variable_dcids="var1",
             select=["variable"],
@@ -61,7 +61,7 @@ def test_observation_payload_validate():
         )  # Requires either `entity_dcids` or `entity_expression`
 
     with pytest.raises(ValueError):
-        ObservationPayload(
+        ObservationRequestPayload(
             date="LATEST",
             variable_dcids="var1",
             select=["value"],  # Missing required "variable" and "entity"
@@ -69,7 +69,7 @@ def test_observation_payload_validate():
         )
 
     with pytest.raises(ValueError):
-        ObservationPayload(
+        ObservationRequestPayload(
             date="LATEST",
             variable_dcids="var1",
             select=["variable", "entity"],
@@ -79,8 +79,8 @@ def test_observation_payload_validate():
 
 
 def test_observation_payload_to_dict():
-    """Tests ObservationPayload conversion to dictionary."""
-    payload = ObservationPayload(
+    """Tests ObservationRequestPayload conversion to dictionary."""
+    payload = ObservationRequestPayload(
         date="LATEST",
         variable_dcids="var1",
         select=["variable", "entity"],
@@ -95,23 +95,23 @@ def test_observation_payload_to_dict():
 
 
 def test_resolve_payload_normalize():
-    """Tests that ResolvePayload normalizes single and multiple nodes."""
-    payload = ResolvePayload(nodes="node1", expression="expr1")
+    """Tests that ResolveRequestPayload normalizes single and multiple nodes."""
+    payload = ResolveRequestPayload(nodes="node1", expression="expr1")
     assert payload.nodes == ["node1"]
 
-    payload = ResolvePayload(nodes=["node1", "node2"], expression="expr1")
+    payload = ResolveRequestPayload(nodes=["node1", "node2"], expression="expr1")
     assert payload.nodes == ["node1", "node2"]
 
 
 def test_resolve_payload_validate():
-    """Tests that ResolvePayload validates its inputs correctly."""
+    """Tests that ResolveRequestPayload validates its inputs correctly."""
     with pytest.raises(ValueError):
-        ResolvePayload(
+        ResolveRequestPayload(
             nodes="node1", expression=123
         )  # `expression` must be a string
 
 
 def test_resolve_payload_to_dict():
-    """Tests ResolvePayload conversion to dictionary."""
-    payload = ResolvePayload(nodes="node1", expression="expr1")
+    """Tests ResolveRequestPayload conversion to dictionary."""
+    payload = ResolveRequestPayload(nodes="node1", expression="expr1")
     assert payload.to_dict == {"nodes": ["node1"], "property": "expr1"}
