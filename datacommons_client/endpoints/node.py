968a1a01533fd1686eaae8f888bba80a7c2997a3
Jorge Rivera <jorge.rivera@one.org>
2025-01-17 15:55:07 -0600

    `Node` endpoint and tests

--- datacommons_client/endpoints/node.py
+++ datacommons_client/endpoints/node.py
@@ -7,6 +7,7 @@ from datacommons_client.endpoints.response import NodeResponse
 
 
 def _normalize_expression_to_string(expression: str | list[str]) -> str:
+<<<<<<< HEAD
   """Converts a list of expressions to a string."""
   if isinstance(expression, str):
     return expression
@@ -17,10 +18,26 @@ def _normalize_expression_to_string(expression: str | list[str]) -> str:
 
 class NodeEndpoint(Endpoint):
   """Initializes the NodeEndpoint with a given API configuration.
+=======
+    """Converts a list of expressions to a string."""
+    if isinstance(expression, str):
+        return expression
+
+    return (
+        f"[{', '.join(expression)}]"
+        if isinstance(expression, list)
+        else expression
+    )
+
+
+class NodeEndpoint(Endpoint):
+    """Initializes the NodeEndpoint with a given API configuration.
+>>>>>>> 5172973 (`Node` endpoint and tests)
 
     Args:
         api (API): The API instance providing the environment configuration
             (base URL, headers, authentication) to be used for requests.
+<<<<<<< HEAD
         max_pages (Optional[int]|None): Optionally, set the maximum number of pages to fetch.
             Defaults to None, which fetches all the pages.
     """
@@ -33,6 +50,19 @@ class NodeEndpoint(Endpoint):
   def fetch(self, node_dcids: str | list[str],
             expression: str | list[str]) -> NodeResponse:
     """Fetches properties or arcs for given nodes and properties.
+=======
+    """
+
+    def __init__(self, api: API, max_pages: Optional[int] = None):
+        """Initializes the NodeEndpoint with a given API configuration."""
+        super().__init__(endpoint="node", api=api)
+        self.max_pages = max_pages
+
+    def fetch(
+        self, node_dcids: str | list[str], expression: str | list[str]
+    ) -> NodeResponse:
+        """Fetches properties or arcs for given nodes and properties.
+>>>>>>> 5172973 (`Node` endpoint and tests)
 
         Args:
             node_dcids (str | List[str]): The DCID(s) of the nodes to query.
@@ -50,6 +80,7 @@ class NodeEndpoint(Endpoint):
             print(response.data)
             ```
         """
+<<<<<<< HEAD
     # Normalize the input expression
     expression = _normalize_expression_to_string(expression)
 
@@ -64,6 +95,25 @@ class NodeEndpoint(Endpoint):
                             node_dcids: str | list[str],
                             out: bool = True) -> NodeResponse:
     """Fetches all property labels for the given nodes.
+=======
+        # Normalize the input expression
+        expression = _normalize_expression_to_string(expression)
+
+        # Create the payload
+        payload = NodeRequestPayload(
+            node_dcids=node_dcids, expression=expression
+        ).to_dict
+
+        # Make the request and return the response.
+        return NodeResponse.from_json(
+            self.post(payload, max_pages=self.max_pages)
+        )
+
+    def fetch_property_labels(
+        self, node_dcids: str | list[str], out: bool = True
+    ) -> NodeResponse:
+        """Fetches all property labels for the given nodes.
+>>>>>>> 5172973 (`Node` endpoint and tests)
 
         Args:
             node_dcids (str | list[str]): The DCID(s) of the nodes to query.
@@ -78,6 +128,7 @@ class NodeEndpoint(Endpoint):
             print(response.data)
             ```
         """
+<<<<<<< HEAD
     # Determine the direction of the properties.
     expression = "->" if out else "<-"
 
@@ -92,6 +143,22 @@ class NodeEndpoint(Endpoint):
       out: bool = True,
   ) -> NodeResponse:
     """Fetches the values of specific properties for given nodes.
+=======
+        # Determine the direction of the properties.
+        expression = "->" if out else "<-"
+
+        # Make the request and return the response.
+        return self.fetch(node_dcids=node_dcids, expression=expression)
+
+    def fetch_property_values(
+        self,
+        node_dcids: str | list[str],
+        expression: str | list[str],
+        constraints: Optional[str] = None,
+        out: bool = True,
+    ) -> NodeResponse:
+        """Fetches the values of specific properties for given nodes.
+>>>>>>> 5172973 (`Node` endpoint and tests)
 
         Args:
             node_dcids (str | List[str]): The DCID(s) of the nodes to query.
@@ -112,6 +179,7 @@ class NodeEndpoint(Endpoint):
             ```
         """
 
+<<<<<<< HEAD
     # Normalize the input to a string (if it's a list), otherwise use the string as is.
     expression = _normalize_expression_to_string(expression)
 
@@ -129,6 +197,25 @@ class NodeEndpoint(Endpoint):
 
   def fetch_all_classes(self) -> NodeResponse:
     """Fetches all Classes available in the Data Commons knowledge graph.
+=======
+        # Normalize the input to a string (if it's a list), otherwise use the string as is.
+        expression = _normalize_expression_to_string(expression)
+
+        # Construct the expression based on the direction and constraints.
+        if out:
+            expression = f"->{expression}"
+            if constraints:
+                expression += f"{{{constraints}}}"
+        else:
+            expression = f"<-{expression}"
+            if constraints:
+                expression += f"{{{constraints}}}"
+
+        return self.fetch(node_dcids=node_dcids, expression=expression)
+
+    def fetch_all_classes(self):
+        """Fetches all Classes available in the Data Commons knowledge graph.
+>>>>>>> 5172973 (`Node` endpoint and tests)
 
         Returns:
             NodeResponse: The response object containing all statistical variables.
@@ -140,6 +227,12 @@ class NodeEndpoint(Endpoint):
             ```
         """
 
+<<<<<<< HEAD
     return self.fetch_property_values(node_dcids="Class",
                                       expression="typeOf",
                                       out=False)
+=======
+        return self.fetch_property_values(
+            node_dcids="Class", expression="typeOf", out=False
+        )
+>>>>>>> 5172973 (`Node` endpoint and tests)
