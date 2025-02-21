6cdb9ad077eaa9ffb78892e68521a2989acc117e
Jorge Rivera <jorge.rivera@one.org>
2025-02-21 16:07:54 +0100

    Update __init__.py
    Adds convenient access to main functions

---
+++ datacommons_client/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "2.0.0b1"
+"""
+Data Commons Client Package
+
+This package provides a Python client for interacting with the Data Commons API.
+"""
+
+from datacommons_client.client import DataCommonsClient
+from datacommons_client.endpoints.base import API
+from datacommons_client.endpoints.node import NodeEndpoint
+from datacommons_client.endpoints.observation import ObservationEndpoint
+from datacommons_client.endpoints.resolve import ResolveEndpoint
+
+__all__ = [
+    "DataCommonsClient",
+    "API",
+    "NodeEndpoint",
+    "ObservationEndpoint",
+    "ResolveEndpoint",
+]
