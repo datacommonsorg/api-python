273ebb7ceb6727f1441c5fdb73106dc8bc20826b
Lucy King <lucysking@google.com>
2025-10-21 15:32:48 -0400

    formatting

--- datacommons_client/utils/error_handling.py
+++ datacommons_client/utils/error_handling.py
@@ -87,4 +87,3 @@ class NoDataForPropertyError(DataCommonsError):
   """Raised when there is no data that meets the specified property filters."""
 
   default_message = "No available data for the specified property filters."
-  
